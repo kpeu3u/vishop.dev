197afe8c98c50164250d12fb3efa6977c9817917
Radoslav Georgiev <kpeu3u@gmail.com>
2025-09-04 14:08:50 +0300

    integrate Monolog for advanced logging configuration

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
